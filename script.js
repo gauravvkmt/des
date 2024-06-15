eca3da16a86d32d06bf4f8849fd0d1d3bd4a3cc0
gauravvkmt <160223715+gauravvkmt@users.noreply.github.com>
2024-06-15 11:35:41 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+var carouselWidth = $('.carousel-inner').scrollWidth;
+var cardWidth = $('.carousel-item').width();
+var scrollPosition = 0;
+$('.carousel-control-next').on('click',function(){
+  scrollPosition = scrollPosition + cardWidth;
+  $('.carousel-inner').animate({scrollLeft: scrollPosition},600);
+});
+$('.carousel-control-prev').on('click',function(){
+  scrollPosition = scrollPosition - cardWidth;
+  $('.carousel-inner').animate({scrollLeft: scrollPosition},600);
+});
